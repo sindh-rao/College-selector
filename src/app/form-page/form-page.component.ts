a4c28aaceeed80ea865565ce358a824c1621da51
Michael Goff <magoff2@ncsu.edu>
2017-04-04 15:48:12 -0400

    added updated form. Still need to figure out scrolling

--- src/app/form-page/form-page.component.ts
+++ src/app/form-page/form-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { moveIn, fallIn } from '../router.animations';
 import { MaterialModule } from '@angular/material';
+//Added http import
 import {Http} from '@angular/http';
 import {contentHeaders} from '../headers';
 
@@ -13,13 +14,12 @@ import {contentHeaders} from '../headers';
   host: {'[@moveIn]': ''}
 })
 export class FormPageComponent implements OnInit {
-  flag = true;
-  response: any;
   public directions = [
-    { value: 'North', display: 'North' },
-    { value: 'South', display: 'South' },
-    { value: 'East', display: 'East' },
-    { value: 'West', display: 'West' }
+    { value: '0', display: 'Northeast' },
+    { value: '1', display: 'Northwest' },
+    { value: '2', display: 'Southeast' },
+    { value: '3', display: 'Southwest' },
+    { value: '4', display: 'Central'}
   ];
 
   public categories = [
@@ -29,17 +29,18 @@ export class FormPageComponent implements OnInit {
     { value: 'finance', display: 'Financial Limitations'}
   ];
 
+//Added http instance
   constructor(private router: Router,private http: Http) { }
 
   ngOnInit() {
   }
+//Added this method
   onSubmit(FormData: any){
+	  console.log(FormData.form._value);
     var body = JSON.stringify(FormData.form._value);
    this.http.post('http://localhost:3001/api/formDetails',body,{ headers: contentHeaders })
     .subscribe(response=>{
       console.log(response);
-      this.flag = false;
-      this.response=response.json();
      },
     error=>{
       alert(error);
