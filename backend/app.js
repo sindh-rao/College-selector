729d4f151597b0a71e719bba6a88251cf8c055ab
Michael Goff <magoff2@ncsu.edu>
2017-04-20 17:19:09 -0400

    live results

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,8 @@ var MongoClient = require('mongodb').MongoClient;
     app.use(bodyParser.json());  
 
 
-
+var body1 = [];
 app.post('/api/formDetails', function (req, res) {
-	var body1={"first":"NCSU","second":"UNC","third":"Gatech","fourth":"UCLA"}
     console.log(req.body);
 	var score=0
 	var name=req.body.major
@@ -29,67 +28,61 @@ app.post('/api/formDetails', function (req, res) {
 	var s2=[];
 	console.log(name)
     MongoClient.connect('mongodb://localhost:27017/mydb', function (err, db) {
-  db.collection('collegesfinal').find().toArray(function (err, docs) {
-        docs.forEach(function (doc) {
-			score=0
-			instate=false;
-			var ranks=[req.body.rank3,req.body.rank2,req.body.rank1];
-			
-			if(doc['public(0) /private(1)']==req.body.public_private){
-				  score+=2+ranks.indexOf("public_private");
-				  }
-			
-            if(doc['State']==req.body.state)
-				  instate=true;
-            if(doc['Acceptance rate']>req.body.accRate){
-				  score+=2+ranks.indexOf("accRate");
-				  }
-            if(doc['Location (0=NE 1=NW 2=SE 3=SW 4=C)']==req.body.desired_location){
-				  score+=2+ranks.indexOf("location");
-				  }
-            if(instate && doc['In-State Tuition']<req.body.inMax){
-				  score+=2+ranks.indexOf("tuition");
-				  }
-			else if(!instate && doc['Out-of-state Tuition']<req.body.outMax){
-				  score+=2+ranks.indexOf("tuition");
-				  }
-			score+=(doc['review nalysis score']	/100)+doc['Youtube 2016-till now reviews'];
-			
-			
-            if(doc['In power five no(0)/ yes(1)']== req.body.sports){
-				  score+=2+ranks.indexOf("sports");
-				  }
-			// if(doc['major'].indexOf(req.body.major)>=0){
-			// 	  score+=2+ranks.indexOf("major");
-			// 	  }
-			  
-			  s2.push(score, doc['Schools']);
-			console.log(doc['Schools']+" "+score);
-			score_array.push([score,doc]);
-			
-                     
-        });
-		console.log(score_array);
-		score_array=score_array.sort(function(a,b){return b[0] - a[0];});
-		console.log("\n------------------------------\n");
-		console.log(score_array);
-		body1 = [];
-		for(var i = 0; i < 4; i++) {
-			body1.push(score_array[i][1]);
-		}
-		console.log(body1);
-
-      });1
-              db.close();
-
-});
-
+  		db.collection('collegesfinal').find().toArray(function (err, docs) {
+			docs.forEach(function (doc) {
+				score=0
+				instate=false;
+				var ranks=[req.body.rank3,req.body.rank2,req.body.rank1];
+				
+				if(doc['public(0) /private(1)']==req.body.public_private){
+					score+=2+ranks.indexOf("public_private");
+					}
+				
+				if(doc['State']==req.body.state)
+					instate=true;
+				if(doc['Acceptance rate']>req.body.accRate){
+					score+=2+ranks.indexOf("accRate");
+					}
+				if(doc['Location (0=NE 1=NW 2=SE 3=SW 4=C)']==req.body.desired_location){
+					score+=2+ranks.indexOf("location");
+					}
+				if(instate && doc['In-State Tuition']<req.body.inMax){
+					score+=2+ranks.indexOf("tuition");
+					}
+				else if(!instate && doc['Out-of-state Tuition']<req.body.outMax){
+					score+=2+ranks.indexOf("tuition");
+					}
+				score+=(doc['review nalysis score']	/100)+doc['Youtube 2016-till now reviews'];
+				
+				
+				if(doc['In power five no(0)/ yes(1)']== req.body.sports){
+					score+=2+ranks.indexOf("sports");
+					}
+				// if(doc['major'].indexOf(req.body.major)>=0){
+				// 	  score+=2+ranks.indexOf("major");
+				// 	  }
+				
+				s2.push(score, doc['Schools']);
+				console.log(doc['Schools']+" "+score);
+				score_array.push([score,doc]);				
+        	});
+			score_array=score_array.sort(function(a,b){return b[0] - a[0];});
+			body1 = [];
+			for(var i = 0; i < 4; i++) {
+				body1.push(score_array[i][1]);
+			}
+			console.log(body1);
+    	});
+    	db.close();
+	});
     res.status(200).send(body1);
+});
 
+app.get('/api/results', function(req, res) {
+	res.send(body1);
 });
 
 
-
-    app.listen('3001', function(){
-        console.log('running on 3001...');
-    });
+app.listen('3001', function(){
+	console.log('running on 3001...');
+});
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import { moveIn, fallIn } from '../router.animations';
 import {Http} from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import {contentHeaders} from '../headers';
 
 @Component({
   selector: 'app-results',
@@ -13,13 +14,24 @@ import { MaterialModule } from '@angular/material';
 })
 export class ResultsComponent implements OnInit {
   public results = [
-    {value: '1', display: 'NCSU'},
-    {value: '2', display: 'UNCG'},
-    {value: '3', display: 'UNCW'},
-    {value: '4', display: 'Duke'},
+    // {value: '1', display: 'NCSU'},
+    // {value: '2', display: 'UNCG'},
+    // {value: '3', display: 'UNCW'},
+    // {value: '4', display: 'Duke'},
   ];
 
-  constructor(private router: Router,private http: Http) { }
+  constructor(private router: Router,private http: Http) { 
+    this.http.get('http://localhost:3001/api/results')
+    .subscribe(response=>{
+      console.log(response.json());
+      this.results = response.json();
+     },
+    error=>{
+      alert(error);
+      //this.router.navigateByUrl('/results');
+    });
+
+  }
 
   ngOnInit() {
   }
